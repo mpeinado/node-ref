b4cc710e98c0c0100800fabea521638d48e3a0b3
marco.peinado <mpeinado7@gmail.com>
2018-02-01 20:32:49 -0700

    commiting a bunch of stuff that I forgot to commit

---
+++ express/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+const port = 3000;
+
+var app = express();
+
+// set ejs as view engine
+app.set('view engine', 'ejs');
+
+// Middleware - code the runs between the request and response
+app.use('/assets', express.static('assets'));
+
+app.get('/', function(req, res){
+    //res.sendFile(__dirname + '/index.html');
+    //res.send('This is the home page');
+    // Middleware - code the runs between the request and response
+    res.render('index')
+});
+
+app.get('/contact', function(req, res){
+    console.log(req.query);
+    res.render('contact', {qs: req.query});
+});
+
+app.get('/profile/:name', function(req, res){
+    const data = {
+        age: 20, 
+        job: 'Software Engineer',
+        hobbies: [
+            'eating',
+            'fishing',
+            'running'
+        ]
+    };
+
+    res.render('profile', {person: req.params.name, data: data});
+});
+
+app.listen(port)
---
+++ express/readme.md
@@ -0,0 +1,4 @@
+# Espress
+- Easy and flexible routing system
+- Integrates with many templeting engines
+- Contains a middleware framework
